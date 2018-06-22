541c5ded24e3e74d4af286e8d092ba657caeb7da
Мальков Владимир Владимирович <malkov.v@milandr01.local>
2018-06-22 08:59:30 +0300

    friday commit

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,15 +1,38 @@
 <template>
-  <div id="General" class="flex-col">
-      <p>Завершите проводку</p>
+<div id="GeneralPlaceholder" class="flex">
+  <div id="General" class="flex-col" :ref="chosenPair = AccountValuePairs[ChosenIncomingData(0, 3)]">
+      <overlay v-show="!show"></overlay>
+      Завершите проводку:
+      <p>
+        поступление в {{ chosenPair.value }}
+      </p>
       <div class="widget-placeholder flex">
         <div class="flex-col flex">
-          <div id="div-druggable" class="widget" draggable="true" v-show="show">{{ ChosenIncomingData }}</div>
+          <div 
+          id="div-draggable" 
+          class="widget" 
+          draggable="true" 
+          @drag="OnDrag">
+            <div v-show="show" class="flex">
+              {{ chosenPair.incomingData[ChosenIncomingData(0, 2)] }}
+            </div>
+          </div>
         </div>
 
         <div>
-          <div class="widget" v-for="pair in AccountValuePairs" :key="pair.id" @drop="OnDrag">Счет {{ pair.account }}</div>
+          <div 
+          class="widget" 
+          v-for="pair in AccountValuePairs" 
+          :key="pair.id" 
+          :class="{dragstyle: pair.isDrag}"
+          @dragover.prevent="pair.isDrag = true" 
+          @dragleave="pair.isDrag = false" 
+          @drop="OnDrop">
+            Счет {{ pair.account }}
+          </div>
         </div>
       </div>
+  </div>
   </div>
 </template>
 
@@ -21,32 +44,62 @@ export default {
         id: 0,
         value: 'касса',
         account: '50',
-        incomingData: [ 'зарплата', 'что-то' ]
+        incomingData: [ 'зарплата', 'что-то' ],
+        isDrag: false
       },
       {
         id: 1,
         value: 'расчетные счета',
         account: '51',
-        incomingData: [ 'зачисление', 'зачисление2' ]
+        incomingData: [ 'зачисление', 'зачисление2' ],
+        isDrag: false
       },
       {
         id: 2,
         value: 'товары',
         account: '41',
-        incomingData: [ 'микросхемы', 'корпуса' ]
+        incomingData: [ 'микросхемы', 'корпуса' ],
+        isDrag: false
       }],
       show: true
     }
   },
-  computed: {
-    ChosenIncomingData: function () {
-      return this.AccountValuePairs[Math.random(4).toFixed()].incomingData[Math.random(2).toFixed()]
+  methods: {
+    ChosenIncomingData: function (min, max) {
+      return Math.floor(Math.random() * (max - min)) + min
+    },
+    OnDrag: function () {
+      this.show = false
+    },
+    OnDrop: function () {
+      this.show = true
+      this.isDrag = false
+      this.AccountValuePairs = [{
+        id: 0,
+        value: 'касса!!!',
+        account: '50!!!',
+        incomingData: [ 'зарплата!!!', 'что-то!!!' ],
+        isDrag: false
+      },
+      {
+        id: 1,
+        value: 'расчетные счета!!!',
+        account: '51!!!',
+        incomingData: [ 'зачисление!!!', 'зачисление2!!!' ],
+        isDrag: false
+      },
+      {
+        id: 2,
+        value: 'товары!!!',
+        account: '41!!!',
+        incomingData: [ 'микросхемы!!!', 'корпуса!!!' ],
+        isDrag: false
+      }]
     }
   },
-  methods: {
-    OnDrag: function (event) {
-      alert('DROOOOP')
-      // event.target.style.display = 'none'
+  components: {
+    overlay: {
+      template: '<div class="overlay"></div>'
     }
   }
 }
@@ -54,12 +107,24 @@ export default {
 
 
 <style>
+p {
+    margin-bottom: 0px;
+}
+#div-draggable{
+    cursor: move;
+}
+#GeneralPlaceholder {
+    margin-top: 3em;
+    align-items: center;
+    justify-content: center;
+}
 #General {
     display: flex;
     align-items: center;
     justify-content: center;
     border: 2px solid darkslategray;
     align-self: stretch;
+    width: initial;
 }
 .widget-placeholder {
     background-color: whitesmoke;
@@ -77,8 +142,24 @@ export default {
     margin: 2em;
     align-items: center;
     justify-content: center;
+    z-index: 200;
 }
 .flex-col {
     flex-direction: column;
+}
+.dragstyle {
+    background-color: lightgoldenrodyellow;
+}
+.overlay {
+    display: flex;
+    position: absolute;
+    width: 100%; 
+    height: 100%; 
+    top: 0; 
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.3);
+    z-index: 100;
 }
 </style>
